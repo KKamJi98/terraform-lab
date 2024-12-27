6debe42fe6d960494c06cdcf162e3efe5cf0b43e
KKamJi <xowl5460@naver.com>
2024-12-27 21:14:16 +0900

    you're doing great

--- dev/variables.tf
+++ dev/variables.tf
@@ -3,3 +3,9 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+variable "region" {
+  description = "The AWS region to launch the server in"
+  type        = string
+  default     = "ap-southeast-1"
+}
