403dcf909e7ed104a41ef496e4759d2be78a3992
KKamJi <xowl5460@naver.com>
2025-01-18 13:40:37 +0900

    update outputs

--- dev/eks_managed_node_group/outputs.tf
+++ dev/eks_managed_node_group/outputs.tf
@@ -1,14 +1,14 @@
 output "cluster_primary_security_group_id" {
-  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
-  value       = try(aws_eks_cluster.this[0].vpc_config[0].cluster_security_group_id, null)
+  value = module.eks_managed_node_group.cluster_primary_security_group_id
+  description = "The ID of the primary security group of the EKS cluster"
 }
 
 output "node_security_group_arn" {
   description = "Amazon Resource Name (ARN) of the node shared security group"
-  value       = try(aws_security_group.node[0].arn, null)
+  value       = module.eks_managed_node_group.node_security_group_arn
 }
 
 output "node_security_group_id" {
   description = "ID of the node shared security group"
-  value       = try(aws_security_group.node[0].id, null)
+  value       = module.eks_managed_node_group.node_security_group_id
 }
