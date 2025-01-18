8cd4220b1c30b700340040dd22c9b319ec74f8c9
KKamJi <xowl5460@naver.com>
2025-01-18 13:42:07 +0900

    update outputs

--- dev/eks_managed_node_group/outputs.tf
+++ dev/eks_managed_node_group/outputs.tf
@@ -1,14 +1,14 @@
 output "cluster_primary_security_group_id" {
-  value = module.eks_managed_node_group.cluster_primary_security_group_id
+  value = module.eks.cluster_primary_security_group_id
   description = "The ID of the primary security group of the EKS cluster"
 }
 
 output "node_security_group_arn" {
   description = "Amazon Resource Name (ARN) of the node shared security group"
-  value       = module.eks_managed_node_group.node_security_group_arn
+  value       = module.eks.node_security_group_arn
 }
 
 output "node_security_group_id" {
   description = "ID of the node shared security group"
-  value       = module.eks_managed_node_group.node_security_group_id
+  value       = module.eks.node_security_group_id
 }
