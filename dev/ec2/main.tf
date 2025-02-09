f3fc98f10ea02955241e3dca515f2ef75950490a
KKamJi <xowl5460@naver.com>
2025-02-09 22:21:45 +0900

    add user_data example

--- dev/ec2/main.tf
+++ dev/ec2/main.tf
@@ -68,3 +68,29 @@ resource "aws_instance" "prometheus_ec2" {
   }
 }
 
+##########################################################
+## EC2 Instance (templatefile_example)
+##########################################################
+
+# resource "aws_instance" "templatefile_example" {
+#   ami                    = "ami-0b5511d5304edfc79"
+#   instance_type          = "t4g.small"
+#   subnet_id              = data.terraform_remote_state.basic.outputs.public_subnet_ids[0]
+#   vpc_security_group_ids = [aws_security_group.test_sg.id]
+#   key_name               = data.terraform_remote_state.basic.outputs.key_pair_name
+
+#   # 외부 스크립트 참조
+#   user_data = templatefile(
+#     "${path.module}/user_data/example.tpl",
+#     {
+#       name        = "Alice"
+#       environment = "staging"
+#     }
+#   )
+
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#     Name        = "templatefile_example"
+#   }
+# }
--- dev/ec2/outputs.tf
+++ dev/ec2/outputs.tf
@@ -17,11 +17,19 @@
 ##########################################################
 
 output "prometheus_ec2_instance_id" {
-  value       = aws_instance.prometheus_ec2.id
-  description = "The ID of the EC2 instance"
+ value       = aws_instance.prometheus_ec2.id
+ description = "The ID of the EC2 instance"
 }
 
 output "prometheus_ec2_instance_public_ip" {
-  value       = aws_instance.prometheus_ec2.public_ip
-  description = "The public IP address of the EC2 instance"
+ value       = aws_instance.prometheus_ec2.public_ip
+ description = "The public IP address of the EC2 instance"
 }
+
+##########################################################
+## EC2 Instance (templatefile_example)
+##########################################################
+
+# output "templatefile_ec2_instance_public_ip" {
+#   value = aws_instance.templatefile_example.public_ip
+# }
