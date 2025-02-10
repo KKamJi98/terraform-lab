7a45dd280032e6cb4ca0fa8ed48b64aad4a536ee
War-Oxi <xowl5460@naver.com>
2025-02-10 21:45:46 +0900

    add hcp terraform backend

---
+++ dev/ec2/backend.tf
@@ -0,0 +1,25 @@
+# terraform {
+#   backend "remote" {
+#     # hostname이 Terraform Cloud(TFC) 기본값은 app.terraform.io,
+#     # HCP Terraform은 별도 hostname을 제공할 수 있음
+#     hostname     = "app.terraform.io"
+
+#     # HCP Terraform에서 설정한 Organization, Workspace 이름
+#     organization = "KKamJi"
+
+#     workspaces {
+#       # Workspace 선택
+#       name = "dev_ec2"
+#     }
+#   }
+# }
+
+terraform {
+  cloud {
+    organization = "KKamJi"
+
+    workspaces {
+      name = "dev_ec2"
+    }
+  }
+}
--- dev/ec2/outputs.tf
+++ dev/ec2/outputs.tf
@@ -17,13 +17,13 @@
 ##########################################################
 
 output "prometheus_ec2_instance_id" {
- value       = aws_instance.prometheus_ec2.id
- description = "The ID of the EC2 instance"
+  value       = aws_instance.prometheus_ec2.id
+  description = "The ID of the EC2 instance"
 }
 
 output "prometheus_ec2_instance_public_ip" {
- value       = aws_instance.prometheus_ec2.public_ip
- description = "The public IP address of the EC2 instance"
+  value       = aws_instance.prometheus_ec2.public_ip
+  description = "The public IP address of the EC2 instance"
 }
 
 ##########################################################
