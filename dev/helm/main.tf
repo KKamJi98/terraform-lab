bb512a333f9571d5f3e00659c77e9633c6e3aa41
KKamJi <xowl5460@naver.com>
2025-02-27 12:15:43 +0900

    update deploy charts example

--- dev/helm/main.tf
+++ dev/helm/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx" {
   name       = "my-nginx"                                 # K8s 내 Release 이름
   repository = "oci://registry-1.docker.io/bitnamicharts" # 차트 레포지토리
   chart      = "nginx"                                    # 차트명
-  version    = "18.3.5"                                   # 차트 버전 (원하는 버전)
+  version    = "18.3.6"                                   # 차트 버전 (원하는 버전)
 
   namespace        = "helm-provider-practice" # 배포할 네임스페이스
   create_namespace = true                     # 네임스페이스가 없을 경우 생성
