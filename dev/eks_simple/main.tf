32b6ffe1a8acaf57d0c6f734590c31b97a4569c9
KKamJi <xowl5460@naver.com>
2025-03-06 13:08:20 +0900

    add external-secrets irsa

--- dev/eks_simple/main.tf
+++ dev/eks_simple/main.tf
@@ -113,3 +113,17 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+
+resource "helm_release" "external-secrets" {
+  name             = "external-secrets"
+  namespace        = "external-secrets"
+  create_namespace = true
+
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  version    = "0.14.3"
+  depends_on = [ 
+    kubernetes_service_account.external_secrets_irsa 
+  ]
+}
--- dev/eks_simple/versions.tf
+++ dev/eks_simple/versions.tf
@@ -1,15 +1,38 @@
-provider "aws" {
-  region = var.region
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
 }
 
 terraform {
   required_version = ">= 1.11.0"
-
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.89"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
