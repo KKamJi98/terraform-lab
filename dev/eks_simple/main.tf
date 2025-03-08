cd579b3fa15a19650d2d2492fdd38faf54ad0d66
KKamJi <xowl5460@naver.com>
2025-03-08 21:26:50 +0900

    update depends

--- dev/eks_simple/main.tf
+++ dev/eks_simple/main.tf
@@ -123,7 +123,7 @@ resource "helm_release" "external-secrets" {
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
   version    = "0.14.3"
-  depends_on = [ 
-    kubernetes_service_account.external_secrets_irsa 
+  depends_on = [
+    kubernetes_service_account.external_secrets_irsa
   ]
 }
