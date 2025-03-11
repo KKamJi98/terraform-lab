121bf432ec2939eb74553a80c3bc11dcdfa0462b
KKamJi <xowl5460@naver.com>
2025-03-11 23:50:19 +0900

    update amd to arm ami

--- dev/ec2/main.tf
+++ dev/ec2/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "test_sg" {
 ##########################################################
 
 data "aws_ssm_parameter" "ubuntu_24_04_ami" {
-  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/arm64/hvm/ebs-gp3/ami-id"
 }
 
 data "aws_ami" "ubuntu_24_04" {
