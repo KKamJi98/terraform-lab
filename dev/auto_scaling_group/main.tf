98e358d1cbe66e1e98d29359c60a0374a768370f
KKamJi <xowl5460@naver.com>
2025-03-13 16:41:18 +0900

    add data arm architecture ubuntu ami

--- dev/auto_scaling_group/main.tf
+++ dev/auto_scaling_group/main.tf
@@ -5,6 +5,14 @@ data "aws_ssm_parameter" "ubuntu_24_04_ami" {
   name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
 }
 
+data "aws_ssm_parameter" "ubuntu_24_04_ami_arm64" {
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/arm64/hvm/ebs-gp3/ami-id"
+}
+
+data "aws_ssm_parameter" "ubuntu_22_04_ami_arm64" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/arm64/hvm/ebs-gp3/ami-id"
+}
+
 locals {
   asg_name = "test_asg"
 }
