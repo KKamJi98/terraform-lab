cb2b593ac7938d54a0e1a5fe9c275c08410494fa
KKamJi <xowl5460@naver.com>
2025-04-06 03:05:07 +0900

    saparate helm.tf from main.tf

--- dev/eks_simple/main.tf
+++ dev/eks_simple/main.tf
@@ -98,17 +98,3 @@ module "eks" {
   # 테스트 용에서만 (실제로는 사용하지 않음) - kms 키는 바로 삭제가 안됨
   cluster_encryption_config = {}
 }
-
-
-resource "helm_release" "external-secrets" {
-  name             = "external-secrets"
-  namespace        = "external-secrets"
-  create_namespace = true
-
-  repository = "https://charts.external-secrets.io"
-  chart      = "external-secrets"
-  version    = "0.14.3"
-  depends_on = [
-    kubernetes_service_account.external_secrets_irsa
-  ]
-}
