d4f129b280714eaa10b18716bd42728bde0ad787
KKamJi <xowl5460@naver.com>
2025-04-30 00:50:29 +0900

    add test docker instace arm64 & amd64

--- dev/ec2/main.tf
+++ dev/ec2/main.tf
@@ -36,24 +36,55 @@ resource "aws_security_group" "test_sg" {
 }
 
 ##########################################################
-## EC2 Instance - InfluxDB (Ubuntu24.04, CentOS7)
+## Docker Instances (AMD64, ARM64)
 ##########################################################
-
-resource "aws_instance" "influxdb_ubuntu_24_04" {
-  count                  = 2
-  ami                    = data.aws_ssm_parameter.ubuntu_24_04_ami.value
-  instance_type          = "t2.micro"
-  subnet_id              = data.terraform_remote_state.basic.outputs.public_subnet_ids[0]
-  key_name               = data.terraform_remote_state.basic.outputs.key_pair_name
-  vpc_security_group_ids = [aws_security_group.test_sg.id]
-
-  # Ubuntu용 InfluxDB 설치 스크립트
-  user_data = file("${path.module}/user_data/influxdb_install_ubuntu.sh")
-
-  tags = {
-    Name = "influxdb-${count.index + 1}"
+locals {
+  instance_configs = {
+    amd64 = {
+      ami           = data.aws_ssm_parameter.ubuntu_24_04_ami.value
+      instance_type = "t2.micro"
+    }
+    arm64 = {
+      ami           = data.aws_ssm_parameter.ubuntu_24_04_ami_arm64.value
+      instance_type = "t4g.small"
+    }
   }
 }
+
+resource "aws_instance" "docker_host" {
+  for_each = local.instance_configs
+
+  ami                    = each.value.ami
+  instance_type          = each.value.instance_type
+  subnet_id              = data.terraform_remote_state.basic.outputs.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.test_sg.id]
+  key_name               = data.terraform_remote_state.basic.outputs.key_pair_name
+
+  # Docker,AWS CLI 설치 스크립트
+  user_data = <<-EOF
+              #!/bin/bash
+              set -eux
+
+              # 패키지 인덱스 갱신 및 Docker 설치
+              apt-get update
+              apt-get install -y docker.io
+
+              # AWS CLI 설치
+              snap install aws-cli --classic
+              
+              # Docker 서비스 활성화
+              systemctl enable docker
+              systemctl start docker
+              EOF
+
+  tags = {
+    Name        = "docker-host-${each.key}"
+    Environment = "dev"
+  }
+}
+
+
+
 
 # influxdb_ubuntu_24_04__instance_public_ip = [
 #         "3.34.139.206",
--- dev/ec2/outputs.tf
+++ dev/ec2/outputs.tf
@@ -13,18 +13,32 @@
 # }
 
 ##########################################################
+## EC2 Instance (Docker Host)
+##########################################################
+
+output "docker_host_instance_id" {
+  value       = [for inst in aws_instance.docker_host : inst.id]
+  description = "The IDs of the EC2 instances"
+}
+
+output "docker_host_instance_public_ip" {
+  value       = [for inst in aws_instance.docker_host : inst.public_ip]
+  description = "The public IPs of the EC2 instances"
+}
+
+##########################################################
 ## EC2 Instance (InfluxDB)
 ##########################################################
 
-output "influxdb_ubuntu_24_04_instance_id" {
-  value       = aws_instance.influxdb_ubuntu_24_04[*].id
-  description = "The ID of the EC2 instance"
-}
+# output "influxdb_ubuntu_24_04_instance_id" {
+#   value       = aws_instance.influxdb_ubuntu_24_04[*].id
+#   description = "The ID of the EC2 instance"
+# }
 
-output "influxdb_ubuntu_24_04_instance_public_ip" {
-  value       = aws_instance.influxdb_ubuntu_24_04[*].public_ip
-  description = "The public IP address of the EC2 instance"
-}
+# output "influxdb_ubuntu_24_04_instance_public_ip" {
+#   value       = aws_instance.influxdb_ubuntu_24_04[*].public_ip
+#   description = "The public IP address of the EC2 instance"
+# }
 
 ##########################################################
 ## EC2 Instance (Prometheus)
